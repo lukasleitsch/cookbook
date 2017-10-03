b1f8debb5546dc034eedbb8e7d72c2ed4df8edae
Lukas Leitsch <lukas@leitsch.org>
2017-10-03 12:24:18 +0200

    :fire: remove old files

--- src/assets/js/min/script-min.js
+++
@@ -1,1 +0,0 @@
-$(function(){$("form input[type='search']").focus(),$(".search-button").click(function(){$(".rezept form").fadeIn("fast"),$("form input[type='search']").focus(),$(this).hide()})});
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,9 +1,9 @@
-$(function() {
-  $("form input[type='search']").focus();
+$(function () {
+  $('form input[type="search"]').focus()
 
-  $('.search-button').click(function() {
-  	$('.rezept form').fadeIn('fast');
-  	$("form input[type='search']").focus();
-  	$(this).hide();
-  });
-});
+  $('.search-button').click(function () {
+    $('.rezept form').fadeIn('fast')
+    $('form input[type="search"]').focus()
+    $(this).hide()
+  })
+})
