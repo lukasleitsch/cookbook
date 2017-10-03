d80b32637bd4c4b4021b389aa377967797448919
Lukas Leitsch <lukas@leitsch.org>
2017-10-03 14:13:18 +0200

    :sparkles: add multiplicator field for ingredients

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -6,4 +6,11 @@ $(function () {
     $('form input[type="search"]').focus()
     $(this).hide()
   })
+
+  $('.js-calc').click(function() {
+    var $zutaten = $('.js-zutaten');
+    $zutaten.html($zutaten.html().replace(/(\d+)([^>])/g, function(match, number, append){
+      return number * parseInt($('.js-factor').val()) + append;
+    }));
+  });
 })
--- src/site/templates/rezept.php
+++ src/site/templates/rezept.php
@@ -1,20 +1,36 @@
 <?php snippet('header') ?>
 <div class="container rezept">
 
-	<div class="rezept__wrap">
-	  <div class="zutaten">
-	  		<h2 class="big">Zutaten</h2>
-	  		<?php echo $page->zutaten()->kirbytext() ?>
-	  	
-	  </div>
-	  <div class="zubereitung">
-	  		<h2 class="big">Zubereitung</h2>
-	  		<?php echo $page->zubereitung()->kirbytext() ?>
-	  </div>
-	</div>
-	<div class="edit">
-		<a href="<?php echo url('panel/pages/' . $page->uri() . '/edit') ?>">Rezept bearbeiten</a>
-	</div>
+  <div class="rezept__wrap">
+    <div class="zutaten">
+      <h2 class="big">Zutaten</h2>
+      <div class="js-zutaten">
+        <?php echo $page->zutaten()->kirbytext() ?>
+      </div>
+      <div class="calc">
+        <div class="input-group">
+          <input type="number" class="form-control js-factor"
+                 placeholder="Faktor">
+          <span class="input-group-btn">
+            <button class="btn btn-default js-calc"
+                    type="button">
+              <span class="glyphicon glyphicon-refresh"
+                    aria-hidden="true"></span>
+            </button>
+          </span>
+        </div>
+      </div>
+
+    </div>
+    <div class="zubereitung">
+      <h2 class="big">Zubereitung</h2>
+      <?php echo $page->zubereitung()->kirbytext() ?>
+    </div>
+  </div>
+  <div class="edit">
+    <a href="<?php echo url('panel/pages/' . $page->uri() . '/edit') ?>">Rezept
+      bearbeiten</a>
+  </div>
 </div>
 
 <?php snippet('footer') ?>
